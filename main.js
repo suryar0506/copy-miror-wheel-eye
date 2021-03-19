7df400b36dbb53ba70271f52069afbfc19b85eba
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-03-20 03:08:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,4 +11,21 @@ console.log(event);
 var Content = event.results[0] [0].transcript;
 console.log(Content);
 document.getElementById("textbox").innerHTML = Content;
+speak();
 }
+
+function speak(){
+    synth = window.speechSynthesis
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+Webcam.attach(camera);
+}
+
+Webcam.set({
+    width : 360,
+    height : 260,
+    image_format : 'png',
+    png_quality : 90
+});
+camera = document.getElementById("camera");

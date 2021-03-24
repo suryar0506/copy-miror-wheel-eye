40d39d7322cde170e9edb374327c51287bace618
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-03-25 03:17:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,25 @@ console.log(event);
 var Content = event.results[0] [0].transcript;
 console.log(Content);
 document.getElementById("textbox").innerHTML = Content;
-speak();
+ 
+if (Content == "take my selfie"){
+    console.log("taking a selfie");
+    speak();
+ }
 }
 
 function speak(){
     synth = window.speechSynthesis
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 6 seconds...";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
-Webcam.attach(camera);
+    Webcam.attach(camera);
+
+setTimeout(function()
+{
+    take_a_snapshot();
+    save();
+}, 6000);
 }
 
 Webcam.set({
@@ -29,3 +39,21 @@ Webcam.set({
     png_quality : 90
 });
 camera = document.getElementById("camera");
+
+function take_a_snapshot(){
+    Webcam.snap(function (data_uri) {
+document.getElementById("result").innerHTML = '<img id = "selfie_image" src = " ' + data_uri + ' ">';
+    });
+}
+
+function save(){
+ link = document.getElementById("link");
+ image = document.getElementById("selfie_image").src ;
+ link.href = image;
+ link.click();
+}
+
+
+
+
+
